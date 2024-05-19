3de5c9266cda9cbd4b6d55bd77a88bf50097ff3b
Naris Razafimahatratra <naris.razafimahatratra@gmail.com>
2024-05-19 13:19:49 +0300

    handle user button redirection

--- lib/user/infrastructure/presentation/components/user_button.dart
+++ lib/user/infrastructure/presentation/components/user_button.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sport_events/user/infrastructure/di/riverpod.dart';
 import 'package:sport_events/user/infrastructure/presentation/view/auth/auth_screen.dart';
+import 'package:sport_events/user/infrastructure/presentation/view/profiles/profiles_screen.dart';
 
-class UserButton extends StatelessWidget {
+class UserButton extends ConsumerWidget {
   const UserButton({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentUserId = ref.read(currentUserIdProvider);
+
     return IconButton(
         onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-            return const AuthScreen();
-          }));
+          final screen = currentUserId == null
+              ? const AuthScreen()
+              : ProfilesScreen(userId: currentUserId);
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => screen),
+          );
         },
         icon: const Icon(Icons.person));
   }
